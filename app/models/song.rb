40f23a3516a604233f87021aa9fb1d242cabb7ea
Felipe <felipebeltran.nyc@gmail.com>
2020-07-17 13:46:47 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,7 @@
 class Song < ApplicationRecord
     validates :title, presence: true
     validates :title, uniqueness: {scope: [:release_year, :artist_name]}  
-    validates :released, inclusion: {in: %w(true false)}
-    validates :artist_name, presence: :true
-    validates :release_year, presence: true, unless: -> {released == false}
-    validates :release_year, numericality: {less_than: 2020}
+    validates :released, inclusion: {in: [true, false]}
+    validates :artist_name, presence: true
+    validates :release_year, presence: true, numericality: {less_than: 2020}, unless: -> {released == false}
 end
